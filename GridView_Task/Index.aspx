4559e87fa13228cb2b57073b71afdf3899785de5
rahul-barthwal-buck <rahul.barthwal@buck.com>
2022-01-12 18:56:27 +0530

    Initial Commit

---
+++ GridView_Task/Index.aspx
@@ -0,0 +1,118 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="GridView_Task.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>GridView Task</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"/>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link href="style.css" rel="stylesheet" />
+
+</head>
+<body>
+    <form id="form1" runat="server">
+       <div class="container">
+        <h1 class="display-3">Product Details</h1>
+        <h3 class="h4">Search</h3>
+        <asp:TextBox ID="txtSearch" runat="server" placeholder="Search Product" TextMode="Search"></asp:TextBox>
+        <asp:Button CssClass="btn btn-info" ID="btnSearch" runat="server" Text="Search" OnClick="btnSearch_Click" ToolTip="Click here to search the record" />
+        <br />
+        <br />
+        <asp:Table runat="server" CssClass="table table-borderless InsertUpdate">
+            <asp:TableRow>
+                <asp:TableCell>
+                    <asp:Label ID="lblName" runat="server" Text="Product Name"></asp:Label>
+                </asp:TableCell>
+                <asp:TableCell>
+                     <asp:TextBox ID="txtProductName" runat="server" placeholder="Enter the Product Name"></asp:TextBox>
+                     <asp:RequiredFieldValidator ID="rvfProductName" runat="server" ValidationGroup="InsertUpdate" ErrorMessage="Product Name is required, cannot be blank" Text="*" ControlToValidate="txtProductName" ForeColor="Red"></asp:RequiredFieldValidator>
+                     <asp:RegularExpressionValidator ID="regexProductName" runat="server" ValidationGroup="InsertUpdate" ErrorMessage="Product Name Should Contain Characters and Numbers only or should be greater than 3 and less than or equal 30 Characters" Text="*" ControlToValidate="txtProductName" ValidationExpression="^[0-9a-zA-Z\s]{3,30}$" ForeColor="Red"></asp:RegularExpressionValidator>
+                </asp:TableCell>
+            </asp:TableRow>
+            <asp:TableRow>
+                <asp:TableCell>
+                     <asp:Label ID="lblQuantityPerUnit" runat="server" Text="Quantity Per Unit"></asp:Label>
+                </asp:TableCell>
+                <asp:TableCell>
+                      <asp:TextBox ID="txtQuantityPerUnit" runat="server" placeholder="Enter Quantity Per Unit"></asp:TextBox>
+                      <asp:RequiredFieldValidator ID="rvfQuantityPerUnit" runat="server" ValidationGroup="InsertUpdate" ErrorMessage="Quantity Per Unit is required, cannot be blank" Text="*" ControlToValidate="txtQuantityPerUnit" ForeColor="Red"></asp:RequiredFieldValidator>
+                     <asp:RegularExpressionValidator ID="regexQuantityPerUnit" runat="server" ValidationGroup="InsertUpdate" ErrorMessage="Quantity Should be numbers or should be less than or equal to 9 length of numbers" Text="*" ControlToValidate="txtQuantityPerUnit" ValidationExpression="(?!^[0]*$)^([0-9]{0,9})$" ForeColor="Red"></asp:RegularExpressionValidator>
+                </asp:TableCell>
+            </asp:TableRow>
+            <asp:TableRow>
+                <asp:TableCell>
+                     <asp:Label ID="lblUnitPrice" runat="server" Text="Unit Price"></asp:Label>
+                </asp:TableCell>
+                <asp:TableCell>
+                     <asp:TextBox ID="txtUnitPrice" runat="server" placeholder="Enter Unit Price"></asp:TextBox>
+                     <asp:RequiredFieldValidator ID="rvfUnitPrice" runat="server" ValidationGroup="InsertUpdate" ErrorMessage="Unit Price is required, cannot be blank" Text="*" ControlToValidate="txtUnitPrice" ForeColor="Red"></asp:RequiredFieldValidator>
+                     <asp:RegularExpressionValidator ID="regexUnitPrice" runat="server" ValidationGroup="InsertUpdate" ErrorMessage="Unit Price should be decimal or should maximum 2 decimal place or should be less than or equal to 12 digits total" Text="*" ControlToValidate="txtUnitPrice" ValidationExpression="(?!^[0]*$)^([0-9\.]{2,12})$*" ForeColor="Red"></asp:RegularExpressionValidator>
+                </asp:TableCell>
+            </asp:TableRow>
+            <asp:TableRow>
+                <asp:TableCell>
+                    <asp:Label ID="lblUnitsInStock" runat="server" Text="Units In Stock"></asp:Label>
+                </asp:TableCell>
+                <asp:TableCell>
+                      <asp:TextBox ID="txtUnitsInStock" runat="server" placeholder="Enter Units In Stock"></asp:TextBox>
+                     <asp:RequiredFieldValidator ID="rvfUnitsInStock" runat="server" ValidationGroup="InsertUpdate" ErrorMessage="Units In Stock is required, cannot be blank" Text="*" ControlToValidate="txtUnitsInStock" ForeColor="Red"></asp:RequiredFieldValidator>
+                     <asp:RegularExpressionValidator ID="regexUnitsInStock" runat="server" ValidationGroup="InsertUpdate" ErrorMessage="Units In Stock be numbers or should be less than or equal to 9 length of numbers" Text="*" ControlToValidate="txtUnitsInStock" ValidationExpression="(?!^[0]*$)^([0-9]{0,9})$" ForeColor="Red"></asp:RegularExpressionValidator>
+                </asp:TableCell>
+            </asp:TableRow>
+            <asp:TableRow>
+                <asp:TableCell ColumnSpan="2">
+                    <asp:Button CssClass="btn btn-success" ID="btnInsert" runat="server" Text="Insert" OnClick="btnInsert_Click" ValidationGroup="InsertUpdate" ToolTip="Click here to Insert the record" />
+                    <asp:Button CssClass="btn btn-success" ID="btnUpdate" runat="server" Text="Update" OnClick="btnUpdate_Click" ValidationGroup="InsertUpdate" ToolTip="Click here to Update the record" />
+                    &nbsp;&nbsp;
+                    <asp:Button CssClass="btn btn-primary" ID="btnReset" runat="server" Text="Reset" OnClick="btnReset_Click" ToolTip="Click here to reset changes" />
+                </asp:TableCell>
+            </asp:TableRow>
+        </asp:Table>
+        <asp:ValidationSummary CssClass="alert alert-danger" ID="ValidationSummary1" runat="server" ValidationGroup="InsertUpdate" ForeColor="Red" />
+        <asp:Label ID="lblMessage" ForeColor="Red" runat="server"></asp:Label>
+        <br />
+        <asp:GridView CssClass="table table-hover table-bordered table-light" ID="GridView1" runat="server" AllowPaging="True" AllowSorting="True" AutoGenerateColumns="False" CellPadding="5" CellSpacing="5"
+            PageSize="5" OnRowEditing="grid_EditRow" OnRowCancelingEdit="grid_EditCancelRow" OnRowDeleting="grid_DeleteRow" OnPageIndexChanging="grid_PageIndexChanging" OnSorting="grid_Sorting">
+            <Columns>
+                <asp:TemplateField HeaderText="Product Id" HeaderStyle-CssClass="hiddenId" ItemStyle-CssClass="hiddenId">
+                    <ItemTemplate>
+                        <asp:Label ID="lblProductId" runat="server" Text='<%# Eval("Product_Id") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Product Name" SortExpression="Name">
+                    <ItemTemplate>
+                        <asp:Label ID="lblProductName" runat="server" Text='<%# Eval("Name") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Quantity Per Unit" SortExpression="Quantity_Per_Unit">
+                    <ItemTemplate>
+                        <asp:Label ID="lblQuantityPerUnit" runat="server" Text='<%# Eval("Quantity_Per_Unit") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Unit Price" SortExpression="Unit_Price">
+                    <ItemTemplate>
+                        <asp:Label ID="lblUnitPrice" runat="server" Text='<%# Eval("Unit_Price") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Units In Stock" SortExpression="Units_In_Stock">
+                    <ItemTemplate>
+                        <asp:Label ID="lblUnitsInStock" runat="server" Text='<%# Eval("Units_In_Stock") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Update">
+                    <ItemTemplate>
+                        <asp:LinkButton CssClass="btn btn-success" ID="btnEdit" runat="server" CommandName="Edit" Text="Edit" ToolTip="Click here to edit this record"></asp:LinkButton>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Delete">
+                    <ItemTemplate>
+                        <asp:LinkButton CssClass="btn btn-danger" ID="btnDelete" runat="server" CommandName="Delete" Text="Delete" ToolTip="Click here to delete this record" OnClientClick="return confirm('Once deleted cannot be returned. Are you sure to delete?');"></asp:LinkButton>
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+       </div>
+    </form>
+</body>
+</html>
---
+++ GridView_Task/Index.aspx.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GridView_Task
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        //Making Connection and Declaring required variables
+        private string strConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["GridViewTaskConnectionString"].ConnectionString;
+        private SqlCommand sqlCommand;
+        private SqlDataAdapter sqlDataAdapter;
+        private DataSet dataSet;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // It indicates whether the page is being rendered for the first time or is being loaded in response to a postback.
+            if (!Page.IsPostBack)
+            {
+                BindProductDetails();
+            }
+            btnUpdate.Visible = false;
+            btnInsert.Visible = true;
+        }
+
+        //Create Connection
+        private void CreateConnection()
+        {
+            SqlConnection sqlConnection = new SqlConnection(strConnectionString);
+            sqlCommand = new SqlCommand();
+            sqlCommand.Connection = sqlConnection;
+
+        }
+
+        //Open Connection
+        private void OpenConnection()
+        {
+            sqlCommand.Connection.Open();
+        }
+        //Close Connection
+        private void CloseConnection()
+        {
+            sqlCommand.Connection.Close();
+        }
+        //Dispose Connection
+        private void DisposeConnection()
+        {
+            sqlCommand.Connection.Dispose();
+        }
+
+        //Binding DataTable to GridView
+        private void BindProductDetails()
+        {
+            GridView1.DataSource = getDataTable();
+            GridView1.DataBind();
+
+        }
+
+        //calling Stored procedure to get all data and then filling it with SqlDataAdapter and returning table for binding
+        private DataTable getDataTable()
+        {
+            try
+            {
+                CreateConnection();
+                OpenConnection();
+                sqlCommand.CommandText = "SP_ProductDetails_GridView";
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.Parameters.AddWithValue("@Event", "Select");
+                sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                dataSet = new DataSet();
+                sqlDataAdapter.Fill(dataSet);
+                return dataSet.Tables[0];
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+
+        }
+
+        //To handle the Sorting event of GridView below event handler will called
+        protected void grid_Sorting(object sender, GridViewSortEventArgs e)
+        {
+
+            string sortDirection = string.Empty;
+
+            //SortDirection is actually  enum and this property available in GridViewSortEventArgs and it tell us whether to sort the data in Asc or Desc order
+            //Here first we we check the direction from direction object of SortDirection property where we stored the direction in ViewState and then the sort direction
+            if (direction == SortDirection.Ascending)
+            {
+                direction = SortDirection.Descending;
+                sortDirection = "Desc";
+            }
+            else
+            {
+                direction = SortDirection.Ascending;
+                sortDirection = "Asc";
+            }
+            //DataView provides different views of the data stored in a DataTable and Due to this view of data from DataTable can be customized and DataView can be used to sort, filter, search, insert, update the data in a DataTable.
+            //DataView either take no argument in constructor or it will take DataTable table as argument.
+            DataView dataView = new DataView(getDataTable());
+            //SortExpression is the property available in GridViewSortEventArgs and due to this we can get the SortExpression value of GridView column which is clicked
+            //Here I am using sort functionality of DataView for sorting the DataTable.
+            dataView.Sort = e.SortExpression + " " + sortDirection;
+            //Here storing the dataView as Object in the Session which helps in pagination when sorting applied.
+            Session["SortedView"] = dataView;
+            GridView1.DataSource = dataView;
+            GridView1.DataBind();
+        }
+
+        //Below event handler will handle the paging into the GridView
+        protected void grid_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            //here firstly we check whether the Session SortedDataView is null or not.
+            if (Session["SortedDataView"] != null)
+            {
+                //If not null then we can bind the DataView object present into the Session object into the GridView and according to sorted data pagination will work.
+                GridView1.DataSource = Session["SortedDataView"];
+                GridView1.DataBind();
+            }
+            else
+            {
+                //If null then bind it will the regular DataTable method which is of DataTable type
+                GridView1.DataSource = getDataTable();
+                GridView1.DataBind();
+            }
+        }
+
+        //Here creating a property of SortDirection type which stores the direction value in ViewState
+        public SortDirection direction
+        {
+            get
+            {
+                if (ViewState["dir"] == null)
+                {
+                    ViewState["dir"] = SortDirection.Ascending;
+                }
+                return (SortDirection)ViewState["dir"];
+            }
+            set
+            {
+                ViewState["dir"] = value;
+            }
+        }
+
+        //When user click Insert then this event will run
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                CreateConnection();
+                OpenConnection();
+                sqlCommand.CommandText = "SP_ProductDetails_GridView";
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.Parameters.AddWithValue("@Event", "Add");
+                sqlCommand.Parameters.AddWithValue("@ProductName", Convert.ToString(txtProductName.Text.Trim()));
+                sqlCommand.Parameters.AddWithValue("@QuantityPerUnit", Convert.ToInt32(txtQuantityPerUnit.Text.Trim()));
+                sqlCommand.Parameters.AddWithValue("@UnitPrice", Convert.ToDecimal(txtUnitPrice.Text));
+                sqlCommand.Parameters.AddWithValue("@UnitsInStock", Convert.ToInt32(txtUnitsInStock.Text));
+                int result = Convert.ToInt32(sqlCommand.ExecuteNonQuery());
+                if (result > 0)
+                {
+                    lblMessage.Text = "Record Inserted Successfully...";
+                    lblMessage.ForeColor = System.Drawing.Color.Green;
+                    BindProductDetails();
+                    ClearControls();
+                }
+                else
+                {
+                    lblMessage.Text = "Failed!";
+                    lblMessage.ForeColor = System.Drawing.Color.Red;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+ 
+        
+        protected void grid_EditCancelRow(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            BindProductDetails();
+        }
+
+        //When user clicks Edit button in grid then this event set the values of that row in the form for editing the value
+        protected void grid_EditRow(object sender, GridViewEditEventArgs e)
+        {
+            btnInsert.Visible = false;
+            btnUpdate.Visible = true;
+
+            int RowIndex = e.NewEditIndex;
+            Label prdId = (Label)GridView1.Rows[RowIndex].FindControl("lblProductId");
+            Session["id"] = prdId.Text;
+
+            txtProductName.Text = ((Label)GridView1.Rows[RowIndex].FindControl("lblProductName")).Text.ToString();
+            txtQuantityPerUnit.Text = ((Label)GridView1.Rows[RowIndex].FindControl("lblQuantityPerUnit")).Text.ToString();
+            txtUnitPrice.Text = ((Label)GridView1.Rows[RowIndex].FindControl("lblUnitPrice")).Text.ToString();
+            txtUnitsInStock.Text = ((Label)GridView1.Rows[RowIndex].FindControl("lblUnitsInStock")).Text.ToString();
+
+        }
+
+        //This will clear all the fields of form and lblMessage
+        public void ClearControls()
+        {
+            txtProductName.Text = "";
+            txtQuantityPerUnit.Text = "";
+            txtUnitPrice.Text = "";
+            txtUnitsInStock.Text = "";
+            lblMessage.Text = "";
+        }
+
+        //When user clicks on Update button then below event will peform and update the fields and then Bind the gridview again.
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                CreateConnection();
+                OpenConnection();
+
+                sqlCommand.CommandText = "SP_ProductDetails_GridView";
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.Parameters.AddWithValue("@Event", "Update");
+                sqlCommand.Parameters.AddWithValue("@ProductName", Convert.ToString(txtProductName.Text.Trim()));
+                sqlCommand.Parameters.AddWithValue("@QuantityPerUnit", Convert.ToInt32(txtQuantityPerUnit.Text.Trim()));
+                sqlCommand.Parameters.AddWithValue("@UnitPrice", Convert.ToDecimal(txtUnitPrice.Text));
+                sqlCommand.Parameters.AddWithValue("@UnitsInStock", Convert.ToInt32(txtUnitsInStock.Text));
+                sqlCommand.Parameters.AddWithValue("@ProductId", Convert.ToDecimal(Session["id"]));
+
+                int result = Convert.ToInt32(sqlCommand.ExecuteNonQuery());
+                if (result > 0)
+                {
+                    lblMessage.Text = "Record Updated Successfully...";
+                    lblMessage.ForeColor = System.Drawing.Color.Green;
+                    GridView1.EditIndex = -1;
+                    BindProductDetails();
+                    ClearControls();
+                }
+                else
+                {
+                    lblMessage.Text = "Failed!";
+                    lblMessage.ForeColor = System.Drawing.Color.Red;
+                }
+                CloseConnection();
+                DisposeConnection();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        //This event will call a function which clear all fields of the form when user clicks on Reset button
+        protected void btnReset_Click(object sender, EventArgs e)
+        {
+            ClearControls();
+        }
+
+        //This event will perform when user clicks on Delete link Button of a particular row and checking the DataKeyNames property of Gridview and according to DataKeyNames which is set to Product_Id and delete record of that row and Bind the GridView again.
+        protected void grid_DeleteRow(object sender, GridViewDeleteEventArgs e)
+        {
+            try
+            {
+                CreateConnection();
+                OpenConnection();
+                Label id = (Label)GridView1.Rows[e.RowIndex].FindControl("lblProductId");
+                sqlCommand.CommandText = "SP_ProductDetails_GridView";
+                sqlCommand.Parameters.AddWithValue("@Event", "Delete");
+                sqlCommand.Parameters.AddWithValue("@ProductId", Convert.ToInt32(id.Text));
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                int result = Convert.ToInt32(sqlCommand.ExecuteNonQuery());
+                if (result > 0)
+                {
+
+                    lblMessage.Text = "Record Deleted Successfully...";
+                    lblMessage.ForeColor = System.Drawing.Color.Green;
+                    GridView1.EditIndex = -1;
+                    BindProductDetails();
+                }
+                else
+                {
+                    lblMessage.Text = "Failed";
+                    lblMessage.ForeColor = System.Drawing.Color.Red;
+                    BindProductDetails();
+                }
+                CloseConnection();
+                DisposeConnection();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        //This event will peform when user clicks on Search Button and it will find the search result and the Bind the gridview again according to the search result.
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                CreateConnection();
+                OpenConnection();
+
+                sqlCommand.CommandText = "SP_ProductDetails_GridView";
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.Parameters.AddWithValue("@Event", "Search");
+                sqlCommand.Parameters.AddWithValue("@ProductName", txtSearch.Text.Trim());
+                sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                dataSet = new DataSet();
+                sqlDataAdapter.Fill(dataSet);
+                GridView1.DataSource = dataSet;
+                GridView1.DataBind();
+                CloseConnection();
+                DisposeConnection();
+                txtSearch.Text = "";
+                lblMessage.Text = "";
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
